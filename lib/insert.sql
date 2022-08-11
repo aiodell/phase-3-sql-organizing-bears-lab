b70bd37af2bdac64022275293777b7df9a93cd8f
Andrianna <andrianna.odell@gmail.com>
2022-08-11 11:50:26 -0400

    commit tree dfabd70aec761d79b797ca6cb70195407392ade1

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament) VALUES("Mr. Chocolate", 13, "M", "brown", "calm" );
+INSERT INTO bears (name, age, sex, color, temperament) VALUES("Rowdy", 6, "M", "black and brown", "crazy");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES("Tabitha", 2, "F", "white", "shy");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES("Sergeant Brown", 10, "M", "dark brown", "territorial");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES("Melissa", 5, "F", "black", "calm");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES("Grinch", 20, "M", "green", "mean");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES("Wendy", 3, "F", "golden", "friendly");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES(NULL, 10, "F", "brown", "mysterious" );
